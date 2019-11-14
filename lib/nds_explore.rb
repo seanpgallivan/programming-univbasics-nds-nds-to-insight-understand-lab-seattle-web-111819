04a8c5b1fb589387d23fc09a00a1f6556fc55395
Sean Gallivan <seanpgallivan@hotmail.com>
2019-11-14 04:23:21 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -8,7 +8,7 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
-  spiel = directors_database()[0]
+  mov = directors_database()[0][1]
   list = ""
   i = 0
   while i < spiel.count do
